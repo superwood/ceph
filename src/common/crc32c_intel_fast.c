65869172811baa74822b80dfe8f4aa8c9bddad33
Yehuda Sadeh <yehuda@inktank.com>
2013-10-04 14:19:24 -0700

    crc32c: add a special case crc32c NULL buffer handling
    Adding a special case crc32c NULL buffer handling for the intel assembly
    code.
    
    Signed-off-by: Yehuda Sadeh <yehuda@inktank.com>

--- src/common/crc32c_intel_fast.c
+++ src/common/crc32c_intel_fast.c
@@ -4,6 +4,7 @@
 #include "common/crc32c_intel_baseline.h"
 
 extern unsigned int crc32_iscsi_00(unsigned char const *buffer, int len, unsigned int crc);
+extern unsigned int crc32_iscsi_zero_00(unsigned char const *buffer, int len, unsigned int crc);
 
 #ifdef HAVE_GOOD_YASM_ELF64
 
@@ -11,6 +12,10 @@ uint32_t ceph_crc32c_intel_fast(uint32_t crc, unsigned char const *buffer, unsig
 {
 	uint32_t v;
 	unsigned left;
+
+
+	if (!buffer)
+		return crc32_iscsi_zero_00(buffer, len, crc);
 
 	/*
 	 * the crc32_iscsi_00 method reads past buffer+len (because it
